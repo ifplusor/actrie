d62bbb8e1e98f0ee6a7323e1b46db9d8f4c1785e
James Yin <ywhjames@hotmail.com>
2020-03-27 10:36:41 +0800

    add build wheel script for linux.

---
+++ utils/wheel_linux.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+set -e
+
+PLAT=manylinux2010_x86_64
+
+PROJECT_NAME=actrie
+PROJECT_DIR=/opt/actrie/
+PROJECT_DIST=/opt/${PROJECT_NAME}_dist/
+DIST_DIR=/opt/wheelhouse/
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" wheel $PROJECT_DIR -w $PROJECT_DIST
+done
+
+# Bundle external shared libraries into the wheels
+for whl in $PROJECT_DIST/*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w $DIST_DIR
+done
+
+# Install twine
+# /opt/python/cp37-cp37m/bin/pip install --user --upgrade twine
+
+# Upload wheel by twine
+/opt/python/cp37-cp37m/bin/python -m twine upload --repository pypi $DIST_DIR/*
+
+# Install packages and test
+# for PYBIN in /opt/python/*/bin/; do
+#     "${PYBIN}/pip" install $PROJECT_NAME --no-index -f $DIST_DIR
+# done
+
+# List installed packages
+# for PYBIN in /opt/python/*/bin/; do
+#     "${PYBIN}/pip" list
+# done
+
+# Uninstall packages
+# for PYBIN in /opt/python/*/bin/; do
+#     "${PYBIN}/pip" uninstall -y $PROJECT_NAME
+# done
+
+# Upload wheel to test pypi by twine
+# /opt/python/cp37-cp37m/bin/python -m twine upload --repository testpypi $DIST_DIR/*
+
+# Install packages from pypi test
+# for PYBIN in /opt/python/*/bin/; do
+#     "${PYBIN}/pip" install --index-url https://test.pypi.org/simple/ --no-deps $PROJECT_NAME
+# done
